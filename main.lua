fec7d6d86d4df358d3cb70a96b5d08e467f95cba
Melzinoff <antoniomelzinoff@hotmail.fr>
2025-11-29 18:41:17 +0100

    pauline cs menu anim

--- main.lua
+++ main.lua
@@ -1139,7 +1139,8 @@ extraCharacters = {
             [CHAR_ANIM_UNLOCK_DOOR] = 'princess_unlock_door',
             [CHAR_ANIM_SWINGING_BOWSER] = 'princess_swinging_bowser',
             [CHAR_ANIM_HOLDING_BOWSER] = 'princess_holding_bowser',
-            [CHAR_ANIM_GRAB_BOWSER] = 'princess_grab_bowser'
+            [CHAR_ANIM_GRAB_BOWSER] = 'princess_grab_bowser',
+            [CS_ANIM_MENU] = "cs_pauline",
         }
     },
     --------------
